afdf7f4fb465af5f511764e0a44f3c2328243ef2
raashis <167985900+raashis@users.noreply.github.com>
2025-09-06 20:20:12 +0800

    Update trivia.py

--- routes/trivia.py
+++ routes/trivia.py
@@ -1,25 +1,14 @@
-from flask import jsonify
-from routes import app
+# routes/trivia.py
+from flask import Blueprint, jsonify
 
-@app.route('/trivia', methods=['GET'])
+trivia_bp = Blueprint('trivia', __name__)
+
+@trivia_bp.route('/trivia', methods=['GET'])
 def trivia():
     answers = [
-        3, #need to see again
-        1,
-        2, 
-        2,  
-        3,
-        4, 
-        4,  
-        5,  # 8. Capture The Flag -> 2
-        4,  # 9. Filler 1 -> 2
-        3,  # 10. Trading Formula -> 1
-        3,  # 11. Filler (Encore) -> 1
-        2,  # 12. Chase The Flag -> 2
-        2,  # 13. Snakes and Ladders Power Up! -> 2
-        1,  # 14. The Ink Archive -> 1
-        2,  # 15. CoolCode Hacker -> 2
-        3,  # 16. Fog-of-Wall -> 1
-        1   # 17. Filler 2 -> 1
+        3, 1, 2, 2, 3,
+        4, 4, 5, 4, 3,
+        3, 2, 2, 1, 2,
+        3, 1
     ]
     return jsonify({"answers": answers})
