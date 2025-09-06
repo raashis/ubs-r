ac151a364bc1e14f26948bae7b1986b9f3b4f6ca
raashis <167985900+raashis@users.noreply.github.com>
2025-09-06 18:55:33 +0800

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.trivia import trivia_bp
-
+from routes.spy_network import spy_network_bp  
 app = Flask(__name__)
 app.register_blueprint(trivia_bp)
+app.register_blueprint(spy_network_bp)  
