a41777c2b1d2514a717e4e541aa7eaad7b830588
raashis <167985900+raashis@users.noreply.github.com>
2025-09-06 14:49:27 +0800

    Update and rename surveillance.py to trivia.py

---
+++ routes/trivia.py
@@ -0,0 +1,25 @@
+from flask import jsonify
+from routes import app
+
+@app.route('/trivia', methods=['GET'])
+def trivia():
+    answers = [
+        1, #need to see again
+        1,
+        2, 
+        2,  
+        3,
+        4, 
+        4,  
+        5,  # 8. Capture The Flag -> 2
+        4,  # 9. Filler 1 -> 2
+        3,  # 10. Trading Formula -> 1
+        3,  # 11. Filler (Encore) -> 1
+        2,  # 12. Chase The Flag -> 2
+        2,  # 13. Snakes and Ladders Power Up! -> 2
+        1,  # 14. The Ink Archive -> 1
+        2,  # 15. CoolCode Hacker -> 2
+        3,  # 16. Fog-of-Wall -> 1
+        1   # 17. Filler 2 -> 1
+    ]
+    return jsonify({"answers": answers})
