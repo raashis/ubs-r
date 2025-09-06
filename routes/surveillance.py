ba6283b8617109f8cae6435fb316091d39e8fc7c
raashis <167985900+raashis@users.noreply.github.com>
2025-09-06 18:55:04 +0800

    Update and rename trivia.py to surveillance.py

---
+++ routes/surveillance.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, request, jsonify
+
+spy_network_bp = Blueprint('spy_network', __name__)
+
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+
+    def find(self, x):
+        if x not in self.parent:
+            self.parent[x] = x
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX == rootY:
+            return False  # Cycle detected
+        self.parent[rootY] = rootX
+        return True
+
+
+@spy_network_bp.route('/investigate', methods=['POST'])
+def investigate():
+    data = request.get_json()
+
+    results = []
+    for network_obj in data:
+        network_id = network_obj["networkId"]
+        connections = network_obj["network"]
+
+        uf = UnionFind()
+        extra_channels = []
+
+        for edge in connections:
+            spy1 = edge["spy1"]
+            spy2 = edge["spy2"]
+            if not uf.union(spy1, spy2):
+                extra_channels.append(edge)
+
+        results.append({
+            "networkId": network_id,
+            "extraChannels": extra_channels
+        })
+
+    return jsonify({"networks": results})
